d4d23d4c08d54d69e8e3b190a7fcc45c68709462
berkegemenoguz <berkegemenoguz@hotmail.com>
2026-05-08 11:02:25 +0300

    Added API routes with Swagger documentation.

---
+++ backend/swagger.js
@@ -0,0 +1,21 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Subscription Tracker API',
+      version: '1.0.0',
+      description: 'RESTful API for tracking personal subscriptions',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local development server',
+      },
+    ],
+  },
+  apis: ['./src/subscriptions.js'],
+};
+
+module.exports = swaggerJsdoc(options);
